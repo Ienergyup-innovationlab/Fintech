50bdd93a61f8c21230737913b40f3526287ef347
Michael ogozi <michealogozi@gmail.com>
2023-11-27 20:17:37 +0300

    added login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      font1: ["inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
