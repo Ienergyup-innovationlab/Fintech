78f91eb9dc68eedc05c0a3755453868e5a5285c1
Michael ogozi <michealogozi@gmail.com>
2023-11-27 19:30:09 +0300

    added the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import SplashScreen from "./components/splashScreen";
-
+import Login from "./components/login";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <SplashScreen />
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
