09f45cf525d71b27d2e6d2bdca22bc715fffa3b1
Michael ogozi <michealogozi@gmail.com>
2023-11-29 13:50:23 +0300

    Revert "updates"
    This reverts commit d4d138f7a6a094b55613ed7ed30aaec10c6ef234.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        purple: "#805DB3",
-        slade: "#F4F4F3",
-      },
-    },
+    extend: {},
     fontFamily: {
       font1: ["inter", "sans-serif"],
-      libre: ["Libre Baskerville ", "serif"],
-      nanum: ["Nanum Gothic", "sans-serif"],
     },
   },
   plugins: [],
