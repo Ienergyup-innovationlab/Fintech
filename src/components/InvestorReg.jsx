70cb1167b6529c029341d52cbe2de72c1bd560c1
shakiogunlesi <shaki_ogunlesi@outlook.com>
2023-12-05 10:13:17 +0100

    Worked on frame 9 and 10
    Worked on frame 9 and 10

--- src/components/InvestorReg.jsx
+++ src/components/InvestorReg.jsx
@@ -10,53 +10,65 @@ function InvestPreference() {
     return (
         <>
             <div className="grid-rows-1 investor-hero"></div>
-            <div className="grid-rows-1 mt-4 justify-center px-6">
+            <div className="grid-rows-1 mt-4 justify-center px-6" id="ellipse">
                 <form>
                     <div className="pt-10">
                         <div>
-                            <h1 className="block text-2xl font-semibold text-gray-800 mb-6">
-                            Investment Preference <span className="text-red">*</span>
+                            <h1 className="block text-2xl text-center font-semibold text-gray-800 mb-6 pl-10">
+                            INVESTOR REGISTRATION <br />FORM
                             </h1>
                         </div>
                         
                         <div>
                             <h1 className="block text-2xl font-semibold text-gray-800 mb-6 mt-6">
-                            Financial Information <span className="text-red">*</span>
+                            Personal Information <span className="text-red">*</span>
                             </h1>
                         </div>
                         <section className="w-full flex flex-col">
                             <input
                             type="text"
-                            id="netWort"
-                            name="netWort"
+                            id="fullName"
+                            name="names"
                             className="py-4 border-2 rounded-lg w-full px-2 border border-transparent bg-grey mb-10 text-black"
                             required
-                            aria-describedby="networt-error"
-                            placeholder="Net Worth"
+                            aria-describedby="fullName-error"
+                            placeholder="&#xf007; &nbsp; Full Name"
+                            />
+                        </section>
+
+                        <section className="w-full flex flex-col">
+                            <input
+                            type="email"
+                            id="emailAdd"
+                            name="email"
+                            className="py-4 border-2 rounded-lg w-full px-2 bg-grey border border-transparent mb-10 text-black"
+                            required
+                            aria-describedby="emailAddress-error"
+                            placeholder="&#xf0e0; &nbsp; Email Address"
+                            />
+                        </section>
+
+                        <section className="w-full flex flex-col">
+                            <input
+                            type="phone"
+                            id="phoneNum"
+                            name="phone"
+                            className="py-4 border-2 rounded-lg w-full px-2 bg-grey border border-transparent mb-10 text-black"
+                            required
+                            aria-describedby="phoneNumber-error"
+                            placeholder="&#x260E; &nbsp; Phone Number"
                             />
                         </section>
 
                         <section className="w-full flex flex-col">
                             <input
                             type="text"
-                            id="investExp"
-                            name="investExp"
-                            className="py-4 border-2 rounded-lg w-full px-2 bg-grey border border-transparent mb-10 text-black"
+                            id="homeAdd"
+                            name="homeAddress"
+                            className="py-4 border-2 rounded-lg w-full px-2 border border-transparent bg-grey mb-10 text-black"
                             required
-                            aria-describedby="investExp-error"
-                            placeholder="Investment Experience"
-                            />
-                        </section>
-
-                        <section className="w-full flex flex-col">
-                            <input
-                            type="text"
-                            id="sourceFound"
-                            name="sourceFound"
-                            className="py-4 border-2 rounded-lg w-full px-2 bg-grey border border-transparent mb-10 text-black"
-                            required
-                            aria-describedby="sourceFound-error"
-                            placeholder="Source Of Funds"
+                            aria-describedby="fhomeAddress-error"
+                            placeholder="&#61461; &nbsp; Residential Address"
                             />
                         </section>
 
