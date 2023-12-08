8d235f45672f4fa0eafcdd2059e77926794297d0
aBugInTheSystem <ekom338.com@gmail.com>
2023-12-08 15:11:05 +0100

    Business Investor Pages
    Created the page one and two for the business investor page. Created a the slider. Code clean up and organisation. Mobile and desktop responsive.

--- src/components/BusinessAccount/BusinessInvestorForm.jsx
+++ src/components/BusinessAccount/BusinessInvestorForm.jsx
@@ -1,6 +1,25 @@
-import Button from "../LandingPage/components/Button";
+import { useState } from "react";
+
+import PageOne from "./PageOne";
+import PageTwo from "./PageTwo";
 
 export default function BusinessInvestorForm() {
+  const [step, setStep] = useState(1);
+
+  const handleNext = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handlePrevious = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  // To be used later for submitting the form
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  // };
+
   return (
     <div className="flex flex-col lg:flex-row lg:justify-between">
       <div className="lg:w-1/2">
@@ -11,48 +30,12 @@ export default function BusinessInvestorForm() {
         />
       </div>
       <div className="md:w-1/2">
-        <h1 className="font-nanum text-center text-2xl font-bold mt-5 lg:mt-10 lg:text-2xl ">
-          Business Registration Form
-        </h1>
-        <div className="pl-7 lg:px-9">
-          <div className="flex mt-9">
-            <h2 className="">Business Information</h2>
-            <p className="text-brickRed">*</p>
-          </div>
-          <form action="" className=" space-y-7 flex flex-col mb-10 ">
-            <input
-              type="text"
-              placeholder="Business name"
-              className="bg-inputGrey mt-3 w-[345px] h-[56px] p-5 focus:outline-none focus:ring focus:ring-purple lg:w-[600px]"
-            />
-            <input
-              type="email"
-              placeholder="Business email address"
-              className="bg-inputGrey w-[345px] h-[56px] p-5 focus:outline-none focus:ring focus:ring-purple lg:w-[600px]"
-            />
-            <input
-              type="text"
-              placeholder="Business phone number"
-              className="bg-inputGrey w-[345px] h-[56px] p-5 focus:outline-none focus:ring focus:ring-purple lg:w-[600px]"
-            />
-
-            <input
-              type="text"
-              placeholder="Industry/Sector"
-              className="bg-inputGrey w-[345px] h-[56px] p-5 focus:outline-none focus:ring focus:ring-purple lg:w-[600px] "
-            />
-            <label htmlFor="Date of establishment">Date of establishment</label>
-            <input
-              type="date"
-              placeholder="Date of establishment"
-              className="bg-inputGrey w-[345px] h-[56px] p-5 focus:outline-none focus:ring focus:ring-purple lg:w-[600px] "
-            />
-            <Button
-              text="Next"
-              variant="bg-purple w-[200px] text-center py-2 px-2 mx-auto text-white font-naunum mt-10 "
-            />
-          </form>
-        </div>
+        <form>
+          {step == 1 && <PageOne handleNext={handleNext} />}
+          {step == 2 && (
+            <PageTwo handleNext={handleNext} handlePrevious={handlePrevious} />
+          )}
+        </form>
       </div>
     </div>
   );
