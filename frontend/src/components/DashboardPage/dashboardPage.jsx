6dcb0a7a4adbe89dc4ee1a71f08deac2555dc6e7
shakiogunlesi <shaki_ogunlesi@outlook.com>
2024-02-28 17:05:25 +0100

    Update dashboardPage.jsx

--- frontend/src/components/DashboardPage/dashboardPage.jsx
+++ frontend/src/components/DashboardPage/dashboardPage.jsx
@@ -120,7 +120,9 @@ const DashboardPage = () => {
                         <div className="font-semibold text-lg">
                         <button type="button" 
                             className="px-5 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <Link to="/">
                             Invest
+                            </Link>
                         </button>
                         </div>
                     </div>
