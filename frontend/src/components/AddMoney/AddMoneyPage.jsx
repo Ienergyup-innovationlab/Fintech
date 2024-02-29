46c536876c12db33f20f5aa7dd3ba84e11cae191
shakiogunlesi <shaki_ogunlesi@outlook.com>
2024-02-29 12:18:47 +0100

    updated

--- frontend/src/components/AddMoney/AddMoneyPage.jsx
+++ frontend/src/components/AddMoney/AddMoneyPage.jsx
@@ -1,15 +1,22 @@
 import ArrowBack from "../images/arrow-back.png";
-
+import 
 import { Link } from "react-router-dom";
 
 
 const AddmoneyPage = () => {
 
+    const [copyText,setCopyText]=useState('')
+
+    const handleCopy=()=>{
+        navigator.clipboard.writeText(copyText)
+        alert("copied")
+    }
+
     return (
       <>
         <div className="bg-lightpurple">
             <div className="px-20 pd-mobile bg-white py-4 drop-shadow-md">
-                <div className="w-1/2 justify-between flex">
+                <div className="w-full justify-between flex">
                     <div className="text-primary">
                         <Link to="/DashboardPage">
                             <img src={ArrowBack} alt="" />
@@ -19,13 +26,16 @@ const AddmoneyPage = () => {
                 </div>
             </div>
             <hr  />
-            <div className="text-center">
-                <div className="my-4 py-2 px-8 rounded-[10px]">
+            <div className="">
+                <div className="my-12 mx-20 py-2 px-8 rounded-[16px] bg-white">
                     <div className="flex flex-row justify-between gap-8">
-                        <div className="grid flex w-full md:w-2/5 mt-6 py-6 px-12">
+                        <div className="grid flex w-full md:w-1/2 mt-6 py-6 px-12">
+                            <p>Account No:</p>
+                            <p className="text-md font-semibold mt-2" value={copyText} onChange={(e)=>setCopyText(e.target.value)}>0987654321
                             
+                            </p>
                         </div>
-                        <div className="w-full flex-wrap md:w-3/5 mt-12 px-10">
+                        <div className="w-full flex-wrap md:w-1/2 mt-12 px-10">
                         
                         </div>
                     </div>
