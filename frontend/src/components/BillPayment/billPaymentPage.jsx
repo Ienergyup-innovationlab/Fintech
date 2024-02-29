521f298e54b80c9551e868f2551839a7bd2333dc
shakiogunlesi <shaki_ogunlesi@outlook.com>
2024-02-29 09:45:44 +0100

    Remove AddMoneyPage
    Remove AddMoneyPage

--- frontend/src/components/BillPayment/billPaymentPage.jsx
+++ frontend/src/components/BillPayment/billPaymentPage.jsx
@@ -24,61 +24,61 @@ const billPaymentPage = () => {
         <div className="grid grid-cols-3 gap-4 text-center my-12">
             <div>
                 <div className="flex justify-center items-center pt-4">
-                    <Link to="/">
+                    <Link to="/PaymentMethodPage">
                     <img src={Electricity} alt="" className="bill-img" />
                     </Link>
                 </div>
-                <Link to="/">
+                <Link to="/PaymentMethodPage">
                 <p className="font-bold text-lg pt-4">Electricity</p>
                 </Link>
             </div>
             <div>
                 <div className="flex justify-center items-center pt-4">
-                    <Link to="/">
+                    <Link to="/PaymentMethodPage">
                     <img src={Tv} alt="" className="bill-img" />
                     </Link>
                 </div>
-                <Link to="/">
+                <Link to="/PaymentMethodPage">
                 <p className="font-bold text-lg pt-4">TV</p>
                 </Link>
             </div>
             <div>
                 <div className="flex justify-center items-center pt-4">
-                    <Link to="/">
+                    <Link to="/PaymentMethodPage">
                     <img src={Internet} alt="" className="bill-img" />
                     </Link>
                 </div>
-                <Link to="/">
+                <Link to="/PaymentMethodPage">
                 <p className="font-bold text-lg pt-4">Internet</p>
                 </Link>
             </div>
             <div>
                 <div className="flex justify-center items-center pt-10">
-                    <Link to="/">
+                    <Link to="/PaymentMethodPage">
                     <img src={Transportation} alt="" className="bill-img" />
                     </Link>
                 </div>
-                <Link to="/">
+                <Link to="/PaymentMethodPage">
                 <p className="font-bold text-lg pt-4">Transportation</p>
                 </Link>
             </div>
             <div>
                 <div className="flex justify-center items-center pt-10">
-                    <Link to="/">
+                    <Link to="/PaymentMethodPage">
                     <img src={Education} alt="" className="bill-img" />
                     </Link>
                 </div>
-                <Link to="/">
+                <Link to="/PaymentMethodPage">
                 <p className="font-bold text-lg pt-4">Education</p>
                 </Link>
             </div>
             <div>
                 <div className="flex justify-center items-center pt-10">
-                    <Link to="/">
+                    <Link to="/PaymentMethodPage">
                     <img src={TravelHotel} alt="" className="bill-img" />
                     </Link>
                 </div>
-                <Link to="/">
+                <Link to="/PaymentMethodPage">
                 <p className="font-bold text-lg pt-4">Travel & Hotel</p>
                 </Link>
             </div>
