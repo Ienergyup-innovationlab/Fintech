3114fc1602aa7583fe3353646b9a3e28023dfa6e
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-04-01 04:37:45 -0700

    Update InvestorRegistration.jsx

--- frontend/src/components/InvestorReg/InvestorRegistration.jsx
+++ frontend/src/components/InvestorReg/InvestorRegistration.jsx
@@ -1,11 +1,53 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 export const InvestorReg = () => {
+  const [formData, setFormData] = useState({
+    names: '',
+    email: '',
+    phone: '',
+    homeAddress: '',
+    // Add other fields as needed
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    try {
+      // Make API call using fetch
+      const response = await fetch('your-api-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      // If API call is successful, log the details
+      const responseData = await response.json();
+      console.log('API Response:', responseData);
+
+      // Clear form fields after successful submission if needed
+      setFormData({
+        name: '',
+        email: '',
+        // Clear other fields as needed
+      });
+    } catch (error) {
+      // Handle errors here
+      console.error('API Error:', error);
+    }
+  };
   return (
     <>
       <div className=" md:bg-sbg  min-h-screen bg-cover bg-no-repeat flex flex-col items-center justify-center pb-20">
         <div className="grid-rows-1 investor-hero"></div>
         <div className="m-deskbg">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="pt-10 pr-6 pl-6">
               <div>
                 <h1 className="md:text-4xl  text-xl font-semibold md:text-black text-black font-poppins capitalize pb-4">
@@ -71,6 +113,7 @@ export const InvestorReg = () => {
                 <button
                   type="submit"
                   className="p-4 inline-flex justify-center items-center rounded-lg border border-transparent bg-primary text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-base dark:focus:ring-offset-gray-800"
+                  value='submit'
                 >
                 <Link to="/InvestmentPreference">
                   Next
